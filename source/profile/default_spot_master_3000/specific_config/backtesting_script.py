bf411c8b3dcc53ab6ef697b79b6500d354d14847
Max <max@tfq.at>
2023-04-01 09:58:17 +0000

    update dependencies

--- source/profile/default_spot_master_3000/specific_config/backtesting_script.py
+++ source/profile/default_spot_master_3000/specific_config/backtesting_script.py
@@ -1,9 +1,2 @@
 async def script(ctx):
-    try:
-        from tentacles.Meta.Keywords.matrix_library.basic_tentacles.RunAnalysis.AnalysisMode import (
-            DefaultRunAnalysisMode,
-        )
-
-        return await DefaultRunAnalysisMode().run_analysis_script(ctx)
-    except (ImportError, ModuleNotFoundError):
-        pass
+    pass
